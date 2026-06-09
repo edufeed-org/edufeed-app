0e839d9d10d4e92f108160dba92391c27eead234
Steffen Rörtgen <sroertgen@gmail.com>
2026-06-09 15:18:08 +0200

    feat(amb): render LicenseBadge per encoding in resource view

---
+++ src/lib/components/educational/EncodingRowBadge.svelte
@@ -0,0 +1,13 @@
+<script>
+  import LicenseBadge from '../shared/LicenseBadge.svelte';
+  import { useLicenseForHash } from '$lib/stores/image-license.svelte.js';
+
+  let { sha256 = '' } = $props();
+
+  const getLicense = useLicenseForHash(() => sha256 || null);
+  const license = $derived(getLicense());
+</script>
+
+{#if license}
+  <LicenseBadge licenseEvent={license} class="ml-2" />
+{/if}
